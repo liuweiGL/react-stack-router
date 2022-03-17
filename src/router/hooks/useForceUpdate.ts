33650ca59ad1f000249b8602ea581a82880a2ac8
liuweigl <wei2460@qq.com>
2022-03-18 01:49:31 +0800

    perf: reduce unnecessary rendering

--- src/router/hooks/useForceUpdate.ts
+++ src/router/hooks/useForceUpdate.ts
@@ -1,7 +1,49 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
-export const useForceUpdate = () => {
-  const [, setState] = useState({})
+import { useDidUpdate } from './useDidUpdate'
 
-  return useCallback(() => setState({}), [])
+const EMPTY_ARRAY: any[] = []
+const RENDER_COUNT = { value: 0 }
+const RENDER_QUEUE = new Set<() => void>()
+
+export function useForceUpdate() {
+  const [, setState] = useState([])
+  const unMountRef = useRef(false)
+
+  useEffect(() => {
+    unMountRef.current = false
+    return () => {
+      unMountRef.current = true
+    }
+  }, EMPTY_ARRAY)
+
+  const update = useCallback(() => {
+    if (unMountRef.current) return
+    setState([])
+  }, EMPTY_ARRAY)
+
+  const scheduler = useCallback(() => {
+    console.warn(`RENDER_COUNT.value`, RENDER_COUNT.value)
+    if (RENDER_COUNT.value === 0) {
+      update()
+    } else {
+      RENDER_QUEUE.add(update)
+    }
+  }, EMPTY_ARRAY)
+
+  RENDER_COUNT.value++
+
+  useDidUpdate(() => {
+    if (RENDER_COUNT.value > 0) {
+      RENDER_COUNT.value--
+    }
+    if (RENDER_COUNT.value === 0) {
+      RENDER_QUEUE.forEach(update => {
+        RENDER_QUEUE.delete(update)
+        update()
+      })
+    }
+  })
+
+  return scheduler
 }
