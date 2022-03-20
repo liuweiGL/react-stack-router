4f9372e43bbec2637b86c7d5018c8b9aaafdfe42
liuweigl <wei2460@qq.com>
2022-03-20 23:14:30 +0800

    chore: add comment

--- src/router/hooks/useDidHide.ts
+++ src/router/hooks/useDidHide.ts
@@ -4,6 +4,7 @@ import { RouteContext } from '../context/RouteContext'
 import { RouteStatus } from '../core/route'
 import { Noop } from '../utils/function'
 
+// TODO: fix not working
 export const useDidHide = (cb: Noop<never>) => {
   const { status } = useContext(RouteContext)
 
